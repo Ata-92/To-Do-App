5cf8229f2edaab6b0998c0a6a104df63bceb72f2
Ata-92 <atakal92@gmail.com>
2021-04-07 20:50:47 +0300

    Add icons and style in js

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const input = document.getElementsByTagName("input")[0];
 const button = document.getElementsByTagName("button")[0];
+button.style.cursor = "pointer";
 const ul = document.querySelector(".main__to-dos");
 
-button.addEventListener("click", function () {
+button.addEventListener("click", itemAdd);
+
+
+
+function itemAdd() {
   if (input.value == "") {
     alert("Please type something to do...");
   } else {
     const div = document.createElement("div");
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-
-    const newLi = document.createElement("li");
-    newLi.style.listStyleType = "none";
-    newLi.style.display = "inline";
-    newLi.textContent = input.value;
-    newLi.style.marginLeft = "1rem";
-    newLi.style.color = "black";
-
-    div.appendChild(checkbox);
-    div.appendChild(newLi);
     div.style.width = "38rem";
     div.style.padding = "1rem";
     div.style.margin = "1rem 0";
@@ -27,10 +19,34 @@ button.addEventListener("click", function () {
     div.style.borderRadius = "1rem";
     div.style.textAlign = "left";
 
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.style.display = "inline";
+
+    const newLi = document.createElement("li");
+    newLi.style.listStyleType = "none";
+    newLi.style.display = "inline-block";
+    newLi.textContent = input.value;
+    newLi.style.width = "32rem";
+    newLi.style.marginLeft = "1rem";
+    newLi.style.color = "black";
+    newLi.style.overflow = "auto";
+
+    const bin = document.createElement("img");
+    bin.src = "1001389331.jpg";
+    bin.style.width = "2rem";
+    bin.style.float = "right";
+    bin.style.cursor = "pointer";
+
+    div.appendChild(checkbox);
+    div.appendChild(newLi);
+    div.appendChild(bin);
+
     ul.appendChild(div);
+    input.value = "";
 
     let counter = 0;
-    checkbox.addEventListener("click", function () {
+    checkbox.addEventListener("click", function done() {
       counter++;
       if (counter % 2 == 1) {
         div.style.backgroundColor = "yellowgreen";
@@ -40,5 +56,7 @@ button.addEventListener("click", function () {
         newLi.style.textDecoration = "none";
       }
     });
+
+
   }
-});
+};
